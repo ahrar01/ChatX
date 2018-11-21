69d2e68a237592c9934ab5aa0133d78bceeb7c04
Ahraar <ahraralam2012@gmail.com>
2018-11-21 09:59:20 +0530

    request fragment added added

--- app/src/main/java/com/appswedevelop/mychat/Fragments/RequestsFragment.java
+++ app/src/main/java/com/appswedevelop/mychat/Fragments/RequestsFragment.java
@@ -3,16 +3,31 @@ package com.appswedevelop.mychat.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.appswedevelop.mychat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RequestsFragment extends Fragment {
+
+    String online_user_id;
+    private RecyclerView myRequestsList;
+    private View myMainView;
+    private DatabaseReference FriendsRequestsReference;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersReference;
+
+    private DatabaseReference FriendsDatabaseRef;
+    private DatabaseReference FriendsReqDatabaseRef;
 
 
     public RequestsFragment() {
@@ -24,7 +39,32 @@ public class RequestsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requests, container, false);
+        myMainView = inflater.inflate(R.layout.fragment_requests, container, false);
+
+        myRequestsList = (RecyclerView) myMainView.findViewById(R.id.requests_list);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        online_user_id = mAuth.getCurrentUser().getUid();
+        FriendsRequestsReference = FirebaseDatabase.getInstance().getReference().child("Friend_Requests").child(online_user_id);
+        UsersReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        FriendsDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Friends");
+        FriendsReqDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Friend_Requests");
+
+        myRequestsList.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        myRequestsList.setLayoutManager(linearLayoutManager);
+
+
+        // Inflate the layout for this fragment
+        return myMainView;
     }
 
 }
+
+
