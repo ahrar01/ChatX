c2d7767c2131d02f1dbe39e54f03a1c8b13f9c79
Ahraar <ahraralam2012@gmail.com>
2018-11-25 21:52:51 +0530

    login password

--- app/src/main/java/com/appswedevelop/mychat/Activity/RegisterActivity.java
+++ app/src/main/java/com/appswedevelop/mychat/Activity/RegisterActivity.java
@@ -109,7 +109,7 @@ public class RegisterActivity extends AppCompatActivity {
 
                     HashMap<String, String> userMap = new HashMap<>();
                     userMap.put("name", display_name);
-                    userMap.put("status", "Hi there I'm using  MyChat App.");
+                    userMap.put("status", "Hi there I'm using  ChatX App.");
                     userMap.put("image", "default");
                     userMap.put("thumb_image", "default");
                     userMap.put("device_token", device_token);
